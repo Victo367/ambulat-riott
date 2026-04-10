0d9eabb5605ef001fd0782f541fa10a8cb3db6d5
Aylamst <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>
2026-04-10 16:31:22 -0300

    sidebar

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+export default function Sidebar() {
+return (
+    <aside className="w-64 h-screen bg-cyan-600 text-white flex flex-col justify-between p-4">
+
+    <div>
+        <h1 className="text-xl font-bold mb-8">
+        Ambulatório TT
+        </h1>
+
+        <nav className="flex flex-col gap-4">
+        <Link href="" className="flex items-center gap-2 hover:opacity-80">
+        Página Inicial
+        </Link>
+
+        <Link href="/login" className="flex items-center gap-2 hover:opacity-80">
+        Login
+        </Link>
+        </nav>
+    </div>
+
+    <div className="text-sm">
+        <p> +55 83 8225-7290</p>
+        <p className="mt-2 text-xs opacity-80">
+        © Copyright by TT gender
+        </p>
+    </div>
+
+    </aside>
+);
+}
