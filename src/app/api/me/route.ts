924cec3b211e70d4992981a15b2b14e63729ef68
Edu <marcoseduardoh555@gmail.com>
2026-04-12 16:26:42 -0300

    feat: Adiciona rota GET para recuperar informações do usuário autenticado

---
+++ src/app/api/me/route.ts
@@ -0,0 +1,35 @@
+import { connectDB } from "@/lib/db";
+import User from "@/models/User";
+import { getUserFromRequest } from "@/lib/getUserFromRequest";
+
+export async function GET(req: Request) {
+  try {
+    await connectDB();
+
+    const loggedUser = getUserFromRequest(req);
+
+    if (!loggedUser) {
+      return Response.json(
+        { error: "Não autenticado" },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findById(loggedUser.id).select("-senha -__v");
+
+    if (!user) {
+      return Response.json(
+        { error: "Usuário não encontrado" },
+        { status: 404 }
+      );
+    }
+
+    return Response.json(user);
+
+  } catch (error: any) {
+    return Response.json(
+      { error: error.message },
+      { status: 500 }
+    );
+  }
+}
