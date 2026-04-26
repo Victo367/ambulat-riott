56ac491d1fffb4d463b229cb6568ed98d977ea8f
Edu <marcoseduardoh555@gmail.com>
2026-04-26 17:45:35 -0300

    refactor: Correção na verificação de usuário no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default async function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex">
-        <Sidebar user={user} />
+        <Sidebar user={user ?? null} />
 
         <main className="flex-1 bg-gray-100 p-8 overflow-y-auto ml-64">
           {children}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,12 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { verifyToken } from "./lib/auth";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
 
   const url = request.nextUrl;
 
-  const publicRoutes = ["/","/login"];
+  const publicRoutes = ["/", "/login"];
 
   if (publicRoutes.includes(url.pathname)) {
     return NextResponse.next();
@@ -15,29 +14,6 @@ export function middleware(request: NextRequest) {
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  const user = verifyToken(token);
-
-  if (!user) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  if (url.pathname === "/login" && user) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-
-  // rotas de paciente
-  if (url.pathname.startsWith("/paciente")) {
-    if (user.tipo !== "paciente") {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
-  }
-
-  if (url.pathname.startsWith("/funcionario")) {
-    if (user.tipo !== "funcionario") {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
   }
 
   return NextResponse.next();
